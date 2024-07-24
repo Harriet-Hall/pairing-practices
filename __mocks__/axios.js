cd3aa7a193de8cca8c9f6faffc39cb4688f6a00e
Harriet <harriet.hall@madetech.com>
2024-07-24 11:21:03 +0100

    WIP returns response 200

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -1,2 +1,6 @@
 import mockAxios from 'jest-mock-axios';
+
+mockAxios.mockResponse({status: 300})
+
+
 export default mockAxios;
--- integration/checkbox.test.js
+++ integration/checkbox.test.js
@@ -2,10 +2,13 @@ import mockAxios from "../__mocks__/axios";
 import bestPracticesAPIController from "../src/BestPracticesApiController";
 
 describe("test best practices API call", () => {
-    bestPracticesAPIController()
+  let response = bestPracticesAPIController();
 
   it("calls mockAxios with endpoint", () => {
-    expect(mockAxios.get).toHaveBeenCalledWith("/best-practices")
+    expect(mockAxios.get).toHaveBeenCalledWith("/best-practices");
   });
 
+  it("returns 200 response", () => {
+      expect(response.status).toBe(200);
+  });
 });
